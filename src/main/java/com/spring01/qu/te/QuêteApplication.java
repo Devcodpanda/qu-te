163e26c7822fb73702098c4a75403b8bd661b0ea
Valou <devcodpanda@gmail.com>
2020-06-02 12:12:38 +0200

    spring1

---
+++ src/main/java/com/spring01/qu/te/QuêteApplication.java
@@ -0,0 +1,13 @@
+package com.spring01.qu.te;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuêteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuêteApplication.class, args);
+	}
+
+}
